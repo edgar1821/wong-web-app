36c373af609f664091873e68dd2b6d317bbf171b
Edgar Jean Marco Gamarra Rojas <edgar.gamarra@softtekprojects.com>
2024-03-22 20:52:13 -0500

    reagrupacion sidebar

--- src/Components/Sidebar/index.tsx
+++ src/Components/Sidebar/index.tsx
@@ -1,18 +1,7 @@
 import { Sidebar } from "flowbite-react";
-import { BiBuoy } from "react-icons/bi";
-import {
-  HiArrowSmRight,
-  HiChartPie,
-  HiShoppingBag,
-  HiTable,
-  HiUser,
-  HiViewBoards,
-} from "react-icons/hi";
-import { FaUserDoctor,  } from "react-icons/fa6";
-import { MdOutlineDocumentScanner } from "react-icons/md";
-// import { IoDocumentAttach } from "react-icons/io5";
-import { MdFactory } from "react-icons/md";
-
+import { HiShoppingBag, HiUser } from "react-icons/hi";
+import { FaUserDoctor } from "react-icons/fa6";
+import { MdOutlineDocumentScanner, MdFactory } from "react-icons/md";
 
 import SidebarTheme from "./sidebarTheme";
 function SidebarApp() {
@@ -34,29 +23,10 @@ function SidebarApp() {
           <Sidebar.Item href="#" icon={MdFactory}>
             Ordenes de trabajo
           </Sidebar.Item>
+        </Sidebar.ItemGroup>
+        <Sidebar.ItemGroup>
           <Sidebar.Item href="#" icon={HiUser}>
-            Users
-          </Sidebar.Item>
-          
-        </Sidebar.ItemGroup>
-        <Sidebar.ItemGroup>
-          <Sidebar.Item href="#" icon={HiArrowSmRight}>
-            Sign In
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiTable}>
-            Sign Up
-          </Sidebar.Item>
-
-        </Sidebar.ItemGroup>
-        <Sidebar.ItemGroup>
-          <Sidebar.Item href="#" icon={HiChartPie}>
-            Upgrade to Pro
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiViewBoards}>
-            Documentation
-          </Sidebar.Item>
-          <Sidebar.Item href="#" icon={BiBuoy}>
-            Help
+            Usuarios
           </Sidebar.Item>
         </Sidebar.ItemGroup>
       </Sidebar.Items>
